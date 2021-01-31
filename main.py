9633dd7c236c65b738b006277571f1125b19e80f
divyanshu12 <divyanshu_12@yahoo.in>
2021-01-31 15:14:21 +0530

    added help, current time and greetings features

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from subprocessFile import note
 from calanderFile import get_date, authenticate_google, get_calendar_event
 from voiceCommand import get_audio, speak
+from time import ctime
 
 wakeup = "Jarvis"
 service = authenticate_google()
@@ -11,6 +12,13 @@ while True:
     text = get_audio()
     print("You : ", text)
     if text.count(wakeup.lower()) > 0:
+
+        TIME_STRS = ["what time is it", "what is the time"]
+        for phrase in TIME_STRS:
+            if phrase in text:
+                print("Time : ", ctime())
+                speak(ctime())
+
         CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
         for phrase in CALENDAR_STRS:
             if phrase in text:
@@ -28,5 +36,17 @@ while True:
                 write_down = get_audio()
                 note(write_down)
                 speak("I've made a note of that.")
+
+        if "help" in text:
+            speak("I can help you with google events, making notes and current time")
+
+        elif "good morning" in text:
+            speak("Good Morning Sir")
+
+        elif "how are you" in text:
+            speak("I am fine")
+
+        else:
+            speak("I don't understand")
 
          
